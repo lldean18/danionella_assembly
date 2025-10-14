ba45c7eb57eb0ba8c157e870cba7c5c700b189b3
Laura Dean <lldean18@gmail.com>
2025-10-14 10:45:39 +0100

    updating the duplex basecalling file. Hopefully running now

--- 02_basecall_duplex.bash
+++ 02_basecall_duplex.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Laura Dean
 # file written for running on the UoN HPC Ada
-# 13/10/25
+# 14/10/25
 
 #SBATCH --partition=ampereq
 #SBATCH --nodes=1
@@ -12,9 +12,17 @@
 #SBATCH --time=100:00:00
 #SBATCH --job-name=danio_duplex_basecall
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+#SBATCH --array=1-2
+
 
 # set variables
 wkdir=/gpfs01/home/mbzlld/data/danionella
+config=/gpfs01/home/mbzlld/code_and_scripts/config_files/danionella_duplex_basecalling_array_config.txt
+
+
+## make the config file (beforehand so its not overwritten with every array item)
+#echo "1 /share/deepseq/matt/danionella/danionella_duplex
+#2 /share/deepseq/matt/danionella/danionella_duplex2" > $config
 
 
 # then make a directory for the basecalled files (only if it does not already exist)
@@ -22,28 +30,22 @@ mkdir -p $wkdir/basecalls
 cd $wkdir/basecalls
 
 
-# make a list of the pod5 files
-find /share/deepseq/matt/danionella -type f -name "*.pod5*" > ~/data/danionella/pod5_list.txt
-# make a list from this with only the simplex files
-sed '/duplex/d' ~/data/danionella/pod5_list.txt > ~/data/danionella/pod5_simplex_list.txt
+# extract the directories from the config file
+pod5_dir=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
 
 
 # load cuda module
 module load cuda-12.2.2
 
 
-# basecall the simplex reads
-xargs -a ~/data/danionella/pod5_simplex_list.txt \
-dorado basecaller \
-sup@latest,5mCG_5hmCG > simplex_SUPlatest_calls.bam
+# basecall the duplex reads
+dorado duplex \
+	sup@latest,5mCG_5hmCG \
+	--recursive \
+       	$pod5_dir > duplex_SUP_calls_${SLURM_ARRAY_TASK_ID}.bam
 
 
 
-
-
-
-
-
-
-
+# unload module
+module unload cuda-12.2.2
 
