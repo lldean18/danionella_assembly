8f6798332e3c6b2ab56e58a21c0f8dbca32b7ae3
Laura Dean <lldean18@gmail.com>
2025-10-15 16:01:59 +0100

    finished duplex extraction script ready to try running when basecalling is complete

--- 03_extract_duplex_from_bams.bash
+++ 03_extract_duplex_from_bams.bash
@@ -1,6 +1,20 @@
+#!/bin/bash
+# Laura Dean
+# 15/10/25
+# script written for running on the UoN HPC Ada
+
+#SBATCH --job-name=extract_duplex_reads
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=50g
+#SBATCH --time=12:00:00
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
-
+# create list of files to loop over
+bams=( /gpfs01/home/mbzlld/data/danionella/basecalls/duplex_SUP_calls_1.bam /gpfs01/home/mbzlld/data/danionella/basecalls/duplex_SUP_calls_2.bam )
 
 
 
@@ -8,9 +22,9 @@
 module load samtools-uoneasy/1.18-GCC-12.3.0
 
 
-for bam in
-do
+for bam in ${bams[@]}
 
+do
 
 # extract duplex reads from bam files
 samtools view \
@@ -30,3 +44,8 @@ $bam
 #--write-index \
 #--output ${bam%.*}_simplex.bam \
 #$bam
+
+done
+
+
+
