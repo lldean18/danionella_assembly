ba1abaf4510a2f9432bc3bce3c74fd11cec8c986
Laura Dean <lldean18@gmail.com>
2025-10-16 12:58:02 +0100

    correcting output file path

--- 05_merge_fastq.gz_files.bash
+++ 05_merge_fastq.gz_files.bash
@@ -9,19 +9,23 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=50g
-#SBATCH --time=12:00:00
+#SBATCH --time=2:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
+# set working directory and move to it
+wkdir=/gpfs01/home/mbzlld/data/danionella/basecalls
+
+
 # combine fastq.gz files into one
-cat /gpfs01/home/mbzlld/data/danionella/basecalls/simplex_SUP_calls_1.fastq.gz \
-	/gpfs01/home/mbzlld/data/danionella/basecalls/simplex_SUP_calls_2.fastq.gz \
-	/gpfs01/home/mbzlld/data/danionella/basecalls/duplex_SUP_calls_1_simplex.fastq.gz \
-	/gpfs01/home/mbzlld/data/danionella/basecalls/duplex_SUP_calls_2_simplex.fastq.gz > ALL_simplex.fastq.gz
+cat simplex_SUP_calls_1.fastq.gz \
+	simplex_SUP_calls_2.fastq.gz \
+	duplex_SUP_calls_1_simplex.fastq.gz \
+	duplex_SUP_calls_2_simplex.fastq.gz > ALL_simplex.fastq.gz
 
 
-cat /gpfs01/home/mbzlld/data/danionella/basecalls/duplex_SUP_calls_1_duplex.fastq.gz \
-	/gpfs01/home/mbzlld/data/danionella/basecalls/duplex_SUP_calls_2_duplex.fastq.gz > all_duplex_duplex.fastq.gz
+cat duplex_SUP_calls_1_duplex.fastq.gz \
+	duplex_SUP_calls_2_duplex.fastq.gz > all_duplex_duplex.fastq.gz
 
 
 
