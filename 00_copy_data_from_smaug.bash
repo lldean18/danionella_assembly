66cf4f86671860d7c4b3aeb254cffadc793f8ee3
Laura Dean <lldean18@gmail.com>
2025-10-22 11:51:05 +0100

    updating smaug file paths

--- 00_copy_data_from_smaug.bash
+++ 00_copy_data_from_smaug.bash
@@ -45,12 +45,16 @@ mbzlld@10.157.200.14:/mnt/waterprom/ic_runs/ic_206/danionellaB_ULK114_recut/2024
 
 
 
+
 # ic_205 (duplex)
 rsync -rvh --progress \
-mbzlld@10.157.200.14:
+mbzlld@10.157.200.14:/mnt/waterprom/ic_runs/ic_205/danionellaB_duplex/20240724_1050_1G_PAU74023_daf962d9/pod5 \
+/gpfs01/home/mbzlld/data/danionella/pod5s/fish_B/ic_205/duplex/
 
-
-
+# ic_205 (duplex2)
+rsync -rvh --progress \
+mbzlld@10.157.200.14:/mnt/waterprom/ic_runs/ic_205/danionellaB_duplex2/20240725_1332_1G_PAU74023_69d9c428/pod5 \
+/gpfs01/home/mbzlld/data/danionella/pod5s/fish_B/ic_205/duplex2/
 
 
 
