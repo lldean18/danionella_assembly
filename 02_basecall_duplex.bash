b128e34d400295ca770f6e56664e3d69fdb6c229
Laura Dean <lldean18@gmail.com>
2025-10-23 12:34:37 +0100

    updating to run on the pod5s downloaded directly from the raw data from smaug

--- 02_basecall_duplex.bash
+++ 02_basecall_duplex.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Laura Dean
 # file written for running on the UoN HPC Ada
-# 14/10/25
+# 23/10/25
 
 #SBATCH --partition=ampereq
 #SBATCH --nodes=1
@@ -23,6 +23,9 @@ config=/gpfs01/home/mbzlld/code_and_scripts/config_files/danionella_duplex_basec
 ## make the config file (beforehand so its not overwritten with every array item)
 #echo "1 /share/deepseq/matt/danionella/danionella_duplex
 #2 /share/deepseq/matt/danionella/danionella_duplex2" > $config
+## make it again using the full downoladed dataset
+#echo "1 /gpfs01/home/mbzlld/data/danionella/pod5s/fish_B/ic_205/duplex duplex
+#2 /gpfs01/home/mbzlld/data/danionella/pod5s/fish_B/ic_205/duplex2 duplex2" > $config
 
 
 # then make a directory for the basecalled files (only if it does not already exist)
@@ -32,7 +35,7 @@ cd $wkdir/basecalls
 
 # extract the directories from the config file
 pod5_dir=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
-
+run=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $config)
 
 # load cuda module
 module load cuda-12.2.2
@@ -42,7 +45,7 @@ module load cuda-12.2.2
 dorado duplex \
 	sup@latest,5mCG_5hmCG \
 	--recursive \
-       	$pod5_dir > duplex_SUP_calls_${SLURM_ARRAY_TASK_ID}.bam
+       	$pod5_dir > duplex_SUP_calls_$run.bam
 
 
 
