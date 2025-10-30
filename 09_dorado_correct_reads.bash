c73bee1ff65f26c80c529c247daedcaebc3ea53b
Laura Dean <lldean18@gmail.com>
2025-10-30 20:44:18 +0000

    part way through updating to run with pre downloaded models

--- 09_dorado_correct_reads.bash
+++ 09_dorado_correct_reads.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Laura Dean
-# 17/10/25
+# 30/10/25
 # for running on the UoN HPC Ada
 
 #SBATCH --partition=ampereq
@@ -12,23 +12,14 @@
 #SBATCH --time=100:00:00
 #SBATCH --job-name=dorado_correct
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
-#SBATCH --array=1-4
 
 
 # set variables
 wkdir=/gpfs01/home/mbzlld/data/danionella/basecalls
 cd $wkdir
-config=/gpfs01/home/mbzlld/code_and_scripts/config_files/danionella_dorado_read_correct_array_config.txt
 
-##### GENERATE THE ARRAY CONFIG FILE #####
-## generate list of file paths for the array config
-#find $wkdir -type f -name "*.fastq.gz" > $config
-## for now I manually removed the files I didn't want to run from $config
-## add array numbers to config file
-#awk '{print NR,$0}' $config > ~/tmp && mv ~/tmp $config
-
-# extract the fastq file paths and names from the config file
-fastq=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
+# specify the file of reads to be corrected
+fastq=FishA_ALL_simplex.fastq.gz
 
 
 
