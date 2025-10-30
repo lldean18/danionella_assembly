86c29fb4054f8b8ca7bdc79b3b668cc65d9e1b82
Laura Dean <lldean18@gmail.com>
2025-10-30 10:16:08 +0000

    fixing typo in wkdir so that path to reads remains correct

--- 06_hifiasm_0.25.0_assembly_FISH_A.bash
+++ 06_hifiasm_0.25.0_assembly_FISH_A.bash
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=96
 #SBATCH --mem=1495g
-#SBATCH --time=48:00:00
+#SBATCH --time=168:00:00
 #SBATCH --job-name=danionella_hifiasm0.25.0
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
@@ -25,14 +25,14 @@ wkdir=/gpfs01/home/mbzlld/data/danionella
 # set the attempt number for naming the output directory of each try
 attempt=1
 # then set the reads file that was used in that attempt
-reads=$wkdir/basecalls/ALL_simplex.fastq.gz
+reads=$wkdir/basecalls/FishA_ALL_simplex.fastq.gz
 
 # print a line to the slurm output that says exactly what was done on this run
 echo "This is hifiasm version 0.25.0 running on the file $reads and saving the output to the directory $wkdir/hifiasm_asm$attempt"
 
 # make directory for the assembly & move to it
-mkdir -p $wkdir/hifiasm_asm$attempt
-cd $wkdir/hifiasm_asm$attempt
+mkdir -p $wkdir/fish_A/hifiasm_asm$attempt
+cd $wkdir/fish_A/hifiasm_asm$attempt
 
 # run hifiasm on the simplex reads with the new --ont flag to generate the assembly
 hifiasm \
