44a6a12b18e35a10c824146cbb03b826556b6084
Laura Dean <lldean18@gmail.com>
2025-11-26 16:01:32 +0000

    adding script to assemble with the dorado corrected reads for fish B

---
+++ 11_hifiasm_0.25.0_assembly_FISH_B_corrected_reads.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Laura Dean
+# 26/11/25
+# for running on Ada
+
+#SBATCH --partition=hmemq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=96
+#SBATCH --mem=1495g
+#SBATCH --time=168:00:00
+#SBATCH --job-name=danionella_hifiasm0.25.0
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+source $HOME/.bash_profile
+
+####### PREPARE ENVIRONMENT #######
+# create conda environment
+#conda create --name hifiasm_0.25.0 hifiasm -y
+conda activate hifiasm_0.25.0
+
+
+# set environment variables
+wkdir=/gpfs01/home/mbzlld/data/danionella
+fish=fish_B
+# set the attempt number for naming the output directory of each try
+attempt=2
+# then set the reads file that was used in that attempt
+reads=$wkdir/basecalls/FishB_ALL_simplex_corrected.fa.gz
+
+# print a line to the slurm output that says exactly what was done on this run
+echo "This is hifiasm version 0.25.0 running on the file $reads and saving the output to the directory $wkdir/$fish/hifiasm_asm$attempt"
+
+# make directory for the assembly & move to it
+mkdir -p $wkdir/$fish/hifiasm_asm$attempt
+cd $wkdir/$fish/hifiasm_asm$attempt
+
+# run hifiasm on the simplex reads (without the new --ont flag because it requires fastq input) to generate the assembly
+hifiasm \
+-t 96 \
+-o ONTasm \
+$reads
+
+# convert the final assembly to fasta format
+awk '/^S/{print ">"$2;print $3}' ONTasm.bp.p_ctg.gfa > ONTasm.bp.p_ctg.fasta
+
+# deactivate conda
+conda deactivate
+
+
