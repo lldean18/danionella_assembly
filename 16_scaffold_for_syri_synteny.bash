d80c1f7ee4f2b27fb046e99e577ff3bb6e2703e8
Laura Dean <lldean18@gmail.com>
2026-01-06 10:39:02 +0000

    trying to get mummer to run since it won't work with the output of minimap2

--- 16_scaffold_for_syri_synteny.bash
+++ 16_scaffold_for_syri_synteny.bash
@@ -5,6 +5,7 @@
 # setup env
 conda activate tmux
 tmux new -t syri
+# increased mem and decreased threads as this version of mummer is single threaded but ran OOM with 80G
 srun --partition defq --cpus-per-task 16 --mem 180g --time 24:00:00 --pty bash
 
 wkdir=/gpfs01/home/mbzlld/data/danionella/try2_zfish_synteny
@@ -12,27 +13,27 @@ cd $wkdir
 ref=/gpfs01/home/mbzlld/data/danionella/GCF_049306965.1_GRCz12tu_genomic.fna
 asm=/gpfs01/home/mbzlld/data/danionella/fish_B/hifiasm_asm1/ONTasm.bp.p_ctg.fasta
 
-#############################
-#### produce alignments #####
-#############################
-
-conda activate minimap2
-minimap2 -x asm5 -t 8 $ref $asm > aln.paf
-conda deactivate
-
-# convert the paf alignment file to input tsv for croder
-awk 'BEGIN{OFS="\t"}
-{
-  idy = ($10/$11)*100
-  print \
-    $8,  $9, \
-    $3,  $4, \
-    $7,  $2, \
-    idy, \
-    $6,  $1, \
-    $11, \
-    0
-}' aln.paf > chroder.coords.tsv
+## #############################
+## #### produce alignments #####
+## #############################
+## 
+## conda activate minimap2
+## minimap2 -x asm5 -t 8 $ref $asm > aln.paf
+## conda deactivate
+## 
+## # convert the paf alignment file to input tsv for croder
+## awk 'BEGIN{OFS="\t"}
+## {
+##   idy = ($10/$11)*100
+##   print \
+##     $8,  $9, \
+##     $3,  $4, \
+##     $7,  $2, \
+##     idy, \
+##     $6,  $1, \
+##     $11, \
+##     0
+## }' aln.paf > chroder.coords.tsv
 
 ##############################################
 # produce alignments 2nd attempt with mummer #
