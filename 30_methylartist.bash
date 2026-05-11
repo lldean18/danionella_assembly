7dd881a607ecd2be65eef07bf89f1a65c629334d
Laura Dean <lldean18@gmail.com>
2026-05-11 12:22:28 +0100

    now adding the padding around each gene

--- 30_methylartist.bash
+++ 30_methylartist.bash
@@ -22,6 +22,18 @@ cd /gpfs01/home/mbzlld/data/danionella/fish_B/methylation/methylartist
 # make an array of intervals to loop over
 declare -a array=(ptg000006l:4707900-4710104 ptg000006l:4711028-4713088 ptg000006l:4714103-4715654 ptg000006l:4732395-4734240 ptg000006l:4737914-4739791 ptg000006l:4759332-4760837 ptg000006l:4771319-4772536 ptg000006l:4774530-4775936 ptg000006l:4782447-4789604 ptg000006l:4795517-4798605 ptg000006l:4804187-4806854 ptg000006l:4820982-4822198 ptg000006l:4829332-4830950)
 
+# add the 1000 bp window around each gene location in the array:
+new_array=()
+for region in "${array[@]}"; do
+    contig=${region%%:*}
+    coords=${region#*:}
+    start=${coords%-*}
+    end=${coords#*-}
+    new_start=$((start - 1000))
+    new_end=$((end + 100))
+    new_array+=("${contig}:${new_start}-${new_end}")
+done
+
 for location in ${array[@]}
 do
 # make a plot of the hox genes
