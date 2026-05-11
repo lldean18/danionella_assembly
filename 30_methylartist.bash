6df318e8e1b8848b4ef1056d9bb611d4e9bb6030
Laura Dean <lldean18@gmail.com>
2026-05-11 12:16:12 +0100

    having a go at making each gene plot separately using an array

--- 30_methylartist.bash
+++ 30_methylartist.bash
@@ -19,17 +19,23 @@ conda activate methylartist
 mkdir -p /gpfs01/home/mbzlld/data/danionella/fish_B/methylation/methylartist
 cd /gpfs01/home/mbzlld/data/danionella/fish_B/methylation/methylartist
 
+# make an array of intervals to loop over
+declare -a array=(ptg000006l:4707900-4710104 ptg000006l:4711028-4713088 ptg000006l:4714103-4715654 ptg000006l:4732395-4734240 ptg000006l:4737914-4739791 ptg000006l:4759332-4760837 ptg000006l:4771319-4772536 ptg000006l:4774530-4775936 ptg000006l:4782447-4789604 ptg000006l:4795517-4798605 ptg000006l:4804187-4806854 ptg000006l:4820982-4822198 ptg000006l:4829332-4830950)
+
+for location in ${array[@]}
+do
 # make a plot of the hox genes
 methylartist locus \
 --bams /gpfs01/home/mbzlld/data/danionella/basecalls_methylation_CpG/fish_B/fish_B_simplex_SUP.bam \
---interval ptg000006l:4705906-4718000 \
+--interval $location \
 --gtf /gpfs01/home/mbzlld/data/danionella/braker_sorted_fix.gtf.gz \
 --ref /share/deepseq/shenson/ds1664_Wilkinson/03_medaka/consensus.fasta \
 --motif CG \
 --labelgenes \
---highlight_alpha 0.75 \
+--highlight_alpha 1 \
 --highlight 4707900-4710104,4711028-4713088,4714103-4715654,4732395-4734240,4737914-4739791,4759332-4760837,4771319-4772536,4774530-4775936,4782447-4789604,4795517-4798605,4804187-4806854,4820982-4822198,4829332-4830950 \
---outfile danionella_hox_region_highlight5
+--outfile danionella_hox_region_$location
+done
 
 # # try with region instead
 # methylartist region \
