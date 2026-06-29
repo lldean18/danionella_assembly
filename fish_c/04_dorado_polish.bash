5e924f2a7b0a2a6fe6d2610c2f95e0306f3b7726
Laura Dean <lldean18@gmail.com>
2026-06-29 14:21:35 +0100

    finished script ready to run

--- fish_c/04_dorado_polish.bash
+++ fish_c/04_dorado_polish.bash
@@ -8,7 +8,7 @@
 #SBATCH --gres=gpu:A100-full:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=40
+#SBATCH --cpus-per-task=50
 #SBATCH --mem=256g
 #SBATCH --time=130:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
@@ -18,22 +18,27 @@
 source $HOME/.bash_profile
 module load cuda-12.2.2
 conda activate samtools1.22
-assembly=/gpfs01/home/mbzlld/data/danionella/fish_c/hifiasm_1/.bp.p_ctg.fasta
+mkdir -p /gpfs01/home/mbzlld/data/danionella/fish_c/dorado_polish
+cd /gpfs01/home/mbzlld/data/danionella/fish_c/dorado_polish
+assembly=/gpfs01/home/mbzlld/data/danionella/fish_c/hifiasm_1/fish_c.bp.p_ctg.fasta
+reads=/gpfs01/home/mbzlld/data/danionella/fish_c/basecalls/SUP_fish_c.bam
 
-
-##  # Align reads to a reference using dorado aligner, sort and index
-##  dorado aligner $assembly /share/deepseq/org_one/SNT052/SUP_basecalls/turtle_SUP.bam |
-##  samtools sort --threads 48 > ${assembly%.*}_mapped_reads.bam
-##  samtools index ${assembly%.*}_mapped_reads.bam
+# Align reads to a reference using dorado aligner, sort and index
+dorado aligner $assembly $reads |
+samtools sort --threads 48 > $(basename ${assembly%.*})_mapped_reads.bam
+samtools index $(basename ${assembly%.*})_mapped_reads.bam
 
 
 # polish the draft assembly
 dorado polish \
 --threads 48 \
 --device cuda:all \
-${assembly%.*}_mapped_reads.bam \
-$assembly > /share/deepseq/org_one/SNT052/dorado_polish/$(basename ${assembly%.*})_polished.fasta
+$(basename ${assembly%.*})_mapped_reads.bam \
+$assembly > $(basename ${assembly%.*})_polished.fasta
 
 
+# cleanup env
 module unload cuda-12.2.2
 conda deactivate
+
+
