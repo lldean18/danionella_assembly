1792b025d196353a3ab4a89ef735539c1ea4740b
Laura Dean <lldean18@gmail.com>
2026-06-30 12:33:07 +0100

    updating to run for the fish b and c fastqs combined

--- fish_c/03_hifiasm_assemble_genome.bash
+++ fish_c/03_hifiasm_assemble_genome.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 29/6/26
+# 30/6/26
 
 # script to assemble a genome with hifiasm v 0.25.0
 
@@ -9,7 +9,7 @@
 #SBATCH --cpus-per-task=96
 #SBATCH --mem=1495g
 #SBATCH --time=140:00:00
-#SBATCH --job-name=danionella_assembly
+#SBATCH --job-name=bc_danionella_assembly
 #SBATCH --output=/gpfs01/home/mbzlld/data/danionella/fish_c/hifiasm_1/slurm-%x-%j.out
 
 
@@ -18,19 +18,19 @@ source $HOME/.bash_profile
 #conda create --name hifiasm_0.25.0 hifiasm -y
 conda activate hifiasm_0.25.0
 attempt=1
-mkdir -p /gpfs01/home/mbzlld/data/danionella/fish_c/hifiasm_$attempt
-reads=/gpfs01/home/mbzlld/data/danionella/fish_c/basecalls/SUP_fish_c.fastq.gz
+mkdir -p /gpfs01/home/mbzlld/data/danionella/fish_b_and_c/hifiasm_$attempt
+reads=/gpfs01/home/mbzlld/data/danionella/fish_b_and_c/SUP_fish_b_and_c.fastq.gz
 
 
 # print a line to the slurm output that says exactly what was done on this run
-echo "This is hifiasm version 0.25.0 running on the file $reads and saving the output to the directory /gpfs01/home/mbzlld/data/danionella/fish_c/hifiasm_$attempt"
+echo "This is hifiasm version 0.25.0 running on the file $reads and saving the output to the directory /gpfs01/home/mbzlld/data/danionella/fish_b_and_c/hifiasm_$attempt"
 
 
 # run hifiasm on the simplex reads with the new --ont flag to generate the assembly
 hifiasm \
 -t 96 \
 --ont \
--o fish_c \
+-o fish_b_and_c \
 $reads
 
 # convert the final assembly to fasta format
