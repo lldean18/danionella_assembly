192d0e523601543328f4bd6d601fae65725fa47d
Laura Dean <lldean18@gmail.com>
2026-07-01 10:07:47 +0100

    final script for fish c

--- fish_c/03_hifiasm_assemble_genome.bash
+++ fish_c/03_hifiasm_assemble_genome.bash
@@ -18,26 +18,26 @@ source $HOME/.bash_profile
 #conda create --name hifiasm_0.25.0 hifiasm -y
 conda activate hifiasm_0.25.0
 attempt=1
-mkdir -p /gpfs01/home/mbzlld/data/danionella/fish_b_and_c/hifiasm_$attempt
-cd /gpfs01/home/mbzlld/data/danionella/fish_b_and_c/hifiasm_$attempt
-reads=/gpfs01/home/mbzlld/data/danionella/fish_b_and_c/SUP_fish_b_and_c.fastq.gz
+mkdir -p /gpfs01/home/mbzlld/data/danionella/fish_c/hifiasm_$attempt
+cd /gpfs01/home/mbzlld/data/danionella/fish_c/hifiasm_$attempt
+reads=/gpfs01/home/mbzlld/data/danionella/fish_c/SUP_fish_c.fastq.gz
 
 
 # print a line to the slurm output that says exactly what was done on this run
-echo "This is hifiasm version 0.25.0 running on the file $reads and saving the output to the directory /gpfs01/home/mbzlld/data/danionella/fish_b_and_c/hifiasm_$attempt"
+echo "This is hifiasm version 0.25.0 running on the file $reads and saving the output to the directory /gpfs01/home/mbzlld/data/danionella/fish_c/hifiasm_$attempt"
 
 
 # run hifiasm on the simplex reads with the new --ont flag to generate the assembly
 hifiasm \
 -t 96 \
 --ont \
--o fish_b_and_c \
+-o fish_c \
 $reads
 
 # convert the final assembly to fasta format
-awk '/^S/{print ">"$2;print $3}' fish_b_and_c.bp.p_ctg.gfa > fish_b_and_c.bp.p_ctg.fasta
-awk '/^S/{print ">"$2;print $3}' fish_b_and_c.bp.hap1.p_ctg.gfa > fish_b_and_c.bp.hap1.p_ctg.fasta
-awk '/^S/{print ">"$2;print $3}' fish_b_and_c.bp.hap2.p_ctg.gfa > fish_b_and_c.bp.hap2.p_ctg.fasta
+awk '/^S/{print ">"$2;print $3}' fish_c.bp.p_ctg.gfa > fish_c.bp.p_ctg.fasta
+awk '/^S/{print ">"$2;print $3}' fish_c.bp.hap1.p_ctg.gfa > fish_c.bp.hap1.p_ctg.fasta
+awk '/^S/{print ">"$2;print $3}' fish_c.bp.hap2.p_ctg.gfa > fish_c.bp.hap2.p_ctg.fasta
 
 
 # cleanup env
