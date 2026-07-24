ca966e385d7f71a7426b1e6893a035d97d625334
Laura Dean <lldean18@gmail.com>
2026-07-24 10:50:54 +0100

    adding a first attempt at variant calling for danionella

---
+++ popgen_analysis/04_call_variants.bash
@@ -0,0 +1,59 @@
+#!/bin/bash
+# 24/7/26
+
+# script to call variants from ONT data
+
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=96
+#SBATCH --mem=361g
+#SBATCH --time=90:00:00
+#SBATCH --job-name=Clair3_variant_call
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+#SBATCH --array=1-3
+# --partition=ampere-mq
+# --gres=gpu:A100-mig:1
+
+# setup config
+CONFIG=~/code_and_scripts/config_files/danionella_inds_config.txt
+ind=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $CONFIG)
+
+echo "slurm array = $SLURM_ARRAY_TASK_ID
+calling variants for sample = $ind
+"
+
+
+# setup env
+source $HOME/.bash_profile
+conda activate clair3
+CLAIR3_PATH=/gpfs01/home/mbzlld/software_bin/Clair3
+MODEL_NAME=r1041_e82_400bps_sup_v500
+cd /gpfs01/home/mbzlld/data/danionella/popgen
+mkdir -p variants
+reference=/share/deepseq/shenson/ds1664_Wilkinson/03_medaka/consensus.fasta
+
+
+
+# run clair3 to call variants for each ind
+python3 ${CLAIR3_PATH}/run_clair3.py \
+  --bam_fn=downsampling/${ind}_TO_consensus_flt.bam \
+  --ref_fn=$reference \
+  --threads=48 \
+  --platform="ont" \
+  --model_path="${CLAIR3_PATH}/models/${MODEL_NAME}" \
+  --output=variants/${ind} \
+  --include_all_ctgs \
+  --sample_name=${ind} \
+  --gvcf #\
+#  --use_gpu
+
+# rename the output files to contain the ind name as GLNexus requires this in the joint genotyping step
+mv variants/${ind}/merge_output.gvcf.gz variants/${ind}/merge_output_${ind}.gvcf.gz
+mv variants/${ind}/merge_output.gvcf.gz.tbi variants/${ind}/merge_output_${ind}.gvcf.gz.tbi
+
+
+# cleanup env
+conda deactivate
+
+
