fb01906b93af8821984ff908496a4e00cf18988d
Laura Dean <lldean18@gmail.com>
2026-07-24 10:20:02 +0100

    running final section now I know I want ton downsample to 15X

--- popgen_analysis/01_downsample_reads.bash
+++ popgen_analysis/01_downsample_reads.bash
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=50g
-#SBATCH --time=60:00:00
+#SBATCH --time=40:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 # setup env
@@ -26,6 +26,48 @@ fastqs=(
 /gpfs01/home/mbzlld/data/danionella/basecalls/FishB_ALL_simplex.fastq.gz
 )
 
+# hashing this chunk out because I ran it separately first to determine the coverage I wanted to filter to
+
+###  for fastq in ${fastqs[@]}
+###  do
+###  
+###  echo "running for $fastq
+###  against the reference $reference
+###  "
+###  
+###  # start by mapping the larger read file to the reference
+###  minimap2 \
+###  -ax map-ont \
+###  -t 16 \
+###  $reference \
+###  $fastq |
+###  samtools sort --threads 16 -o $(basename ${fastq%.*.*})_TO_$(basename ${reference%.*}).bam
+###  samtools index --threads 16 $(basename ${fastq%.*.*})_TO_$(basename ${reference%.*}).bam
+###  
+###  # filter to retain only good mappings
+###  samtools view \
+###  -b \
+###  -q 30 \
+###  -F 260 \
+###  $(basename ${fastq%.*.*})_TO_$(basename ${reference%.*}).bam |
+###  samtools sort --threads 16 -o $(basename ${fastq%.*.*})_TO_$(basename ${reference%.*})_flt.bam
+###  samtools index --threads 16 $(basename ${fastq%.*.*})_TO_$(basename ${reference%.*})_flt.bam
+###  
+###  # compute the current coverage of the reference bam
+###  echo "the initial depth of coverage of the well mapped reads is:"
+###  samtools depth \
+###  $(basename ${fastq%.*.*})_TO_$(basename ${reference%.*})_flt.bam |
+###  awk '{sum+=$3} END {print sum/NR}'
+###  
+###  done
+
+# AGAINST concensus.fasta the depths were
+# fish c: 136.855
+# fish a: 14.4006
+# fish b: 65.2899
+
+##############################
+
 for fastq in ${fastqs[@]}
 do
 
@@ -33,60 +75,20 @@ echo "running for $fastq
 against the reference $reference
 "
 
-# start by mapping the larger read file to the reference
-minimap2 \
--ax map-ont \
--t 16 \
-$reference \
-$fastq |
-samtools sort --threads 16 -o $(basename ${fastq%.*.*})_TO_$(basename ${reference%.*}).bam
-samtools index --threads 16 $(basename ${fastq%.*.*})_TO_$(basename ${reference%.*}).bam
-
-# filter to retain only good mappings
-samtools view \
--b \
--q 30 \
--F 260 \
-$(basename ${fastq%.*.*})_TO_$(basename ${reference%.*}).bam |
-samtools sort --threads 16 -o $(basename ${fastq%.*.*})_TO_$(basename ${reference%.*})_flt.bam
-samtools index --threads 16 $(basename ${fastq%.*.*})_TO_$(basename ${reference%.*})_flt.bam
-
-# compute the current coverage of the reference bam
-echo "the initial depth of coverage of the well mapped reads is:"
-samtools depth \
+# randomly subsample to the desired coverage
+rasusa aln \
+--coverage 15 \
 $(basename ${fastq%.*.*})_TO_$(basename ${reference%.*})_flt.bam |
-awk '{sum+=$3} END {print sum/NR}'
+samtools sort --threads 16 -o $(basename ${fastq%.*.*})_downsampled.bam
+samtools index --threads 16 $(basename ${fastq%.*.*})_downsampled.bam
+
+# check final depth
+echo "the final depth of the downsampled bam file is:"
+samtools depth \
+$(basename ${fastq%.*.*})_downsampled.bam \
+| awk '{sum+=$3} END {print sum/NR}'
 
 done
-
-# AGAINST concensus.fasta the depths were
-# fish c: 136.855
-# fish a: 14.4006
-# fish b:
-
-##############################
-
-##  for fastq in ${fastqs[@]}
-##  do
-##  
-##  echo "running for $fastq
-##  against the reference $reference
-##  "
-##  
-##  # randomly subsample to the desired coverage
-##  rasusa aln \
-##  --coverage 22 \
-##  $(basename ${fastq%.*.*})_TO_$(basename ${reference%.*})_flt.bam |
-##  samtools sort --threads 16 -o $(basename ${fastq%.*.*})_downsampled.bam
-##  samtools index --threads 16 $(basename ${fastq%.*.*})_downsampled.bam
-##  
-##  # check final depth
-##  echo "the final depth of the downsampled bam file is:"
-##  samtools depth \
-##  $(basename ${fastq%.*.*})_downsampled.bam \
-##  | awk '{sum+=$3} END {print sum/NR}'
-##  
-##  done
 
 # cleanup env
 conda deactivate
