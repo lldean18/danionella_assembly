407c8c69b9930aba9e7bf57dc139ea499af71e80
Laura Dean <lldean18@gmail.com>
2026-07-28 10:35:48 +0100

    adding mods from yesterday, this seems to have run successfully

--- popgen_analysis/02_call_variants.bash
+++ popgen_analysis/02_call_variants.bash
@@ -12,8 +12,6 @@
 #SBATCH --job-name=Clair3_variant_call
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 #SBATCH --array=1-3
-# --partition=ampere-mq
-# --gres=gpu:A100-mig:1
 
 # setup config
 CONFIG=~/code_and_scripts/config_files/danionella_inds_config.txt
@@ -26,9 +24,15 @@ calling variants for sample = $ind
 
 # setup env
 source $HOME/.bash_profile
-conda activate clair3
+#########
+# install the latest version to fix invalid gvcf output when no variants detected
+#conda create -n clair3_2.0.2 -c conda-forge -c bioconda -y clair3
+conda activate clair3_2.0.2
+#########
+#conda activate clair3
 CLAIR3_PATH=/gpfs01/home/mbzlld/software_bin/Clair3
 MODEL_NAME=r1041_e82_400bps_sup_v500
+#dna_r10.4.1_e8.2_400bps_sup@v5.2.0
 cd /gpfs01/home/mbzlld/data/danionella/popgen
 mkdir -p variants
 reference=/share/deepseq/shenson/ds1664_Wilkinson/03_medaka/consensus.fasta
@@ -36,7 +40,8 @@ reference=/share/deepseq/shenson/ds1664_Wilkinson/03_medaka/consensus.fasta
 
 
 # run clair3 to call variants for each ind
-python3 ${CLAIR3_PATH}/run_clair3.py \
+#python3 ${CLAIR3_PATH}/run_clair3.py \
+run_clair3.sh \
   --bam_fn=downsampling/${ind}_TO_consensus_flt.bam \
   --ref_fn=$reference \
   --threads=48 \
@@ -45,8 +50,7 @@ python3 ${CLAIR3_PATH}/run_clair3.py \
   --output=variants/${ind} \
   --include_all_ctgs \
   --sample_name=${ind} \
-  --gvcf #\
-#  --use_gpu
+  --gvcf
 
 # rename the output files to contain the ind name as GLNexus requires this in the joint genotyping step
 mv variants/${ind}/merge_output.gvcf.gz variants/${ind}/merge_output_${ind}.gvcf.gz
